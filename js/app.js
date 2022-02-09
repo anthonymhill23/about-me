5f611afd8fcf83f32e6148d33b4c191e3a211e59
Anthony Hill <anthonymhill23@gmail.com>
2022-02-08 23:14:23 -0500

    code for css and java and js

---
+++ js/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+console.log ('I am the greatest')
+
+let answerOne = prompt ('Am I am a fan of Star Wars?') .toLowerCase();
+
+if (answerOne === 'yes' || answerOne === 'y') {
+  alert ('This is the way');
+
+
+} else if (answerOne === 'no' || answerOne === 'n') {
+  alert('wrong, I do in fact know the ways of the darkside');
+  
+}  {
+  
+}
